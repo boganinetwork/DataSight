0a6ab239352059c73615230a0f27e5c900736a1e
Bogani Network <bogani.network@gmail.com>
2026-08-10 00:26:14 +0800

    Pakai versi DuckDB-WASM yang lebih baru yang pakai try_table

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as duckdb from "https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/+esm";
+import * as duckdb from "https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@latest/+esm";
 
 const statusEl = document.getElementById("status");
 const runBtn = document.getElementById("run-btn");
